229d00ebdbb308c8c129c699d116a118c39795e1
Thorgalix <lucaspodevin@sfr.fr>
2025-12-09 10:11:43 +0100

    Ajout de la fonction pour compter le nombre de feuilles d'un noeud

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - count leaves of a node
+ * @tree: root node
+ *
+ * Return: total of leaves, or 0 if tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+	return (0);
+
+	if (binary_tree_is_leaf(tree))
+	return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
