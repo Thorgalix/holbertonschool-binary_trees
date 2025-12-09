6c5775cce954c34c344a6bfc98018becaf3f1e9f
Thorgalix <lucaspodevin@sfr.fr>
2025-12-09 10:33:15 +0100

    Ajout de la fonction pour calculer la hauteur de chaque branche

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,10 +13,31 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (0);
 
 	if (tree->left)
-	hleft = 1 + binary_tree_balance(tree->left);
+	hleft = 1 + binary_tree_height(tree->left);
 
 	if (tree->right)
-	hright = 1 + binary_tree_balance(tree->right);
+	hright = 1 + binary_tree_height(tree->right);
 
 	return (hleft - hright);
+}
+/**
+ * binary_tree_height - iterative height of a binary tree
+ * @tree: root node
+ *
+ * Return: height, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t hleft = 0, hright = 0;
+
+	if (!tree)
+	return (0);
+
+	if (tree->left)
+	hleft = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+	hright = 1 + binary_tree_height(tree->right);
+
+	return (hleft > hright ? hleft : hright);
 }
