d19c66627595503685b05c20122c3cf60f33153c
Thorgalix <lucaspodevin@sfr.fr>
2025-12-09 09:58:38 +0100

    Ajout de la fonction pour calculer la taille d'un arbre à partir d'un noeud

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - iterative size of a binary tree
+ * @tree: root node
+ *
+ * Return: size, or 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (!tree)
+	return (0);
+
+	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+
+	return (size);
+}
